586384f85186c19473dae41677221b7f93452a61
uxxin <dbwlsdid1114@naver.com>
2025-07-21 12:46:35 +0900

    Link 컴포넌트를 통해 페이지 네비게이션 하기

--- 6.next-js/next-routing/app/products/page.js
+++ 6.next-js/next-routing/app/products/page.js
@@ -1,11 +1,19 @@
+import Link from "next/link";
+
 // products/page.js, 상품 목록이 보여지는 페이지
-const ProductList = () => {
+const ProductList = (params) => {
+  //상품 목록이 들어있는 리스트 데이터
+  const productList = [3, 4];
+  const { productId } = params;
   return (
     <div>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+
+      {productList.map((productId) => (
+        <h2 key={productId}>
+          <Link href={`products/${productId}`}>Product {productId}</Link>
+        </h2>
+      ))}
     </div>
   );
 };
