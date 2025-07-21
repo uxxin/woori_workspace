672de551b1fbbca74ae52652d0966dfe66dc954b
uxxin <dbwlsdid1114@naver.com>
2025-07-21 16:26:55 +0900

    에러 처리하기

--- 6.next-js/next-routing/app/profile/page.js
+++ 6.next-js/next-routing/app/profile/page.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const page = () => {
+  const random = Math.floor(Math.random() * 2);
+  if (random === 1) {
+    throw new Error("프로필 페이지 로딩 과정에서 에러 발생");
+  }
   return <div>프로필 페이지</div>;
 };
 
