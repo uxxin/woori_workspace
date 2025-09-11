be77d79c46b9ebf0905a91e8eb685877777028b3
uxxin <dbwlsdid1114@naver.com>
2025-09-11 12:03:49 +0900

    프록시 개념 이해: 디자인 패턴 코드 작성

---
+++ 14.spring-aop/step03-logging-aop/src/main/java/dev/aop/App.java
@@ -0,0 +1,13 @@
+package dev.aop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
