53b7ab2c5d011c5f6a62f03357879f4162aa69dd
uxxin <dbwlsdid1114@naver.com>
2025-09-15 23:06:27 +0900

    백엔드 교과목 평가 예제

---
+++ step12simpleapp_with_authorization/src/main/java/dev/syntax/security/controller/UserController.java
@@ -0,0 +1,49 @@
+package dev.syntax.security.controller;
+
+import dev.syntax.security.model.Product;
+import dev.syntax.security.model.User;
+import dev.syntax.security.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String getUsers(Model model) {
+        List<User> users = userService.getUsers();
+
+        model.addAttribute("users", users);
+        return "userList";
+    }
+
+    @PostMapping
+    public void createUser(User user) {
+        if (userService.getUser(user.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Already exists email with =" + user.getEmail());
+        }
+
+        userService.createUser(user);
+    }
+
+    @GetMapping("/mypage")
+    public String showMyPage(Authentication authentication, Model model) {
+
+        // 인증된 사용자의 정보를 담고 있는 객체에서 사용자 이름 취득
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("name", user.getName());
+
+        return "myPage";
+    }
+}
