f664fb35c2d3fa41aa7e416d1946123b3a96dc72
uxxin <dbwlsdid1114@naver.com>
2025-09-22 12:35:45 +0900

    우분투 테스트용 깃허브

---
+++ ubuntu-test/src/main/java/dev/syntax/UbuntuTestApplication.java
@@ -0,0 +1,13 @@
+package dev.syntax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbuntuTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbuntuTestApplication.class, args);
+    }
+
+}
